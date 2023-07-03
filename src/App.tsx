62476e4cda589b84c9651330d94ebbe58e975761
Sergey-717 <94647143+Sergey-717@users.noreply.github.com>
2023-07-03 16:40:33 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ function App() {
               />
             }
           ></Route>
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<Main />} />
         </Routes>
         {modalState && (
           <Modal
